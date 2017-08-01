478c34dd6eb61fc3baf9993eaf0dcd55c823c024
Thomas Kuntz <Thomas.kuntz68@gmail.com>
2017-08-01 14:04:24 +0200

    TP02 Servlet

---
+++ src/main/java/dev/sgp/web/EditerCollaborateurController.java
@@ -0,0 +1,48 @@
+package dev.sgp.web;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EditerCollaborateurController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		String avecMatricule = req.getParameter("matricule");
+
+		resp.setContentType("text/html");
+
+		if (avecMatricule == null) {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.getWriter().write("<h1>Un matricule est attendu</h1>");
+		} else {
+			resp.setStatus(HttpServletResponse.SC_OK);
+			resp.getWriter().write("<h1> Edition de collaborateur</h1>Matricule : " + avecMatricule);
+		}
+	}
+
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		List<String> list = Stream.of("matricule", "titre", "nom", "prenom").filter(p -> req.getParameter(p) == null)
+				.collect(Collectors.toList());
+		resp.getWriter().write("");
+
+		if (list.isEmpty()) {
+			resp.setStatus(HttpServletResponse.SC_CREATED);
+			resp.getWriter()
+					.write("Creation d'un collaborateur avec les informations suivantes : </br> matricule = "
+							+ req.getParameter("matricule") + ", titre = " + req.getParameter("titre") + ", nom = "
+							+ req.getParameter("nom") + ", prenom = " + req.getParameter("prenom"));
+		} else {
+			resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+			resp.getWriter().write("Les parametres suivants sont incorrect : " + String.join(", ", list));
+		}
+	}
+}
